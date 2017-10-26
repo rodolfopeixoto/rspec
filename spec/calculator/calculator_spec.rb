3c58d3b4cd7b0f26ecd7f24a1b9fe140ced3cb65
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-10-26 01:32:30 -0200

    Add sum with number negative

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -6,4 +6,9 @@ describe Calculator do
     result = calc.sum(5,7)
     expect(result).to eq(12)
   end
+  it 'use sum method for 2 numbers with negative numbers' do
+    calc = Calculator.new
+    result = calc.sum(-5,7)
+    expect(result).to eq(2)
+  end
 end
